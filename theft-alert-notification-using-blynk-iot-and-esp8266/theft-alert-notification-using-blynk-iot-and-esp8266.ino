8cf264440c1fafdff3e4c42c03dd23e798a67ff2
Arduinex <techadvancepro10@gmail.com>
2025-07-18 21:03:07 +0530

    Add files via upload

---
+++ theft-alert-notification-using-blynk-iot-and-esp8266/theft-alert-notification-using-blynk-iot-and-esp8266.ino
@@ -0,0 +1,43 @@
+#define BLYNK_TEMPLATE_ID "TMPL3N0oCyca7"
+#define BLYNK_TEMPLATE_NAME "security system"
+#define BLYNK_AUTH_TOKEN "18YYcrl039V_YYYYiBivhuHpBBC0YuPH"
+
+
+#define BLYNK_PRINT Serial
+#include <WiFi.h>
+#include <BlynkSimpleEsp32.h>
+
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+
+char ssid[] = "vishnu";      // your WiFi name
+char pass[] = "8445200@$";   // your WiFi password
+
+
+#define PIR_SENSOR  4        // GPIO pin for PIR sensor
+BlynkTimer timer;
+
+
+void notifyOnTheft() {
+  int isTheftAlert = digitalRead(PIR_SENSOR);
+  Serial.println(isTheftAlert);
+  if (isTheftAlert == 1) {
+    Serial.println("Theft Alert in Home");
+    Blynk.logEvent("security_system", "Theft Alert in Home");
+  }
+}
+
+
+void setup() {
+  pinMode(PIR_SENSOR, INPUT);
+  Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+  timer.setInterval(5000L, notifyOnTheft);
+}
+
+
+void loop() {
+  Blynk.run();
+  timer.run();
+}
